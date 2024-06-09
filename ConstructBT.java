7953eaf2b17128e2f6fe5e3adc52053a1a911c7b
pujakumari <puja.kumari@sjsu.edu>
2024-06-09 14:43:35 -0700

    Completed Trees-1

--- ConstructBT.java
+++ ConstructBT.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class ConstructBT {
+/***
+ TC - O(n)
+ SC - O(h)
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+class ConstructBT {
+
+    Map<Integer, Integer> hm;
+    int idx;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        if(preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0) {
+            return null;
+        }
+
+        //Using hashmap to map inorder array elements to its index
+        hm = new HashMap<>();
+        for(int i =0; i < inorder.length; i++) {
+            hm.put(inorder[i], i);
+        }
+
+        return recurse(preorder, 0, inorder.length-1);
+    }
+
+    public TreeNode recurse(int[] preorder, int start, int end) {
+        //base condition
+        if(start > end)
+            return null;
+
+        int rootVal = preorder[idx++];
+
+        TreeNode root = new TreeNode(rootVal);
+
+        int rootIdx = hm.get(rootVal);
+
+        root.left = recurse(preorder, start, rootIdx-1);
+        root.right = recurse(preorder, rootIdx+1, end);
+
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
--- ValidateBST.java
+++ ValidateBST.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class ValidateBST {
+/**
+ *  TC - O(n), SC - O(h)
+ */
+
+
+class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+
+        if(root == null)
+            return true;
+
+        return isValidBST(root, null, null);
+
+    }
+
+    private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+        if(root == null)
+            return true;
+
+        if(min != null && root.val <= min || max != null && root.val >= max) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
